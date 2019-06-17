535ef1f5dc7c0e0f1e837524cca2bcf1490da36e
Seth Lachman <lachmanseth17@gmail.com>
2019-06-17 18:31:01 -0400

    Added three more tests to project-1 spec.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,4 +1,11 @@
-const { multiplyByTen, subtractFive } = require("./project-1");
+const {
+  multiplyByTen,
+  subtractFive,
+  areSameLength,
+  areEqual,
+  lessThanNinety,
+  greaterThanFifty
+} = require("./project-1");
 
 // start testing!
 describe("Multiply by ten function", () => {
@@ -14,5 +21,45 @@ describe("Subtract by 5 function", () => {
     expect(subtractFive(5)).toBe(0);
     expect(subtractFive(15)).toBe(10);
     expect(subtractFive(0)).toBe(-5);
+  });
+});
+
+describe("Function testing length of strings", () => {
+  it("it should return true if lengths are the same length", () => {
+    expect(areSameLength("semantic", "kingdoms")).toBe(true);
+  });
+
+  it("it should return false if lengths are not the same length", () => {
+    expect(areSameLength("semantic", "kingdom")).toBe(false);
+  });
+});
+
+describe("Function testing equality of numbers", () => {
+  it("it should return true if numbers are equal", () => {
+    expect(areEqual(5, 5)).toBe(true);
+  });
+
+  it("it should return false if numbers are not equal", () => {
+    expect(areEqual(5, 7)).toBe(false);
+  });
+});
+
+describe("Function testing if number is less than 90", () => {
+  it("it should return true if number is less than 90", () => {
+    expect(lessThanNinety(43)).toBe(true);
+  });
+
+  it("it should return false if number is greater than or equal to 90", () => {
+    expect(lessThanNinety(99)).toBe(false);
+  });
+});
+
+describe("Function testing if number is greater than 50", () => {
+  it("it should return true if number is greater than 50", () => {
+    expect(greaterThanFifty(64)).toBe(true);
+  });
+
+  it("it should return false if number is less than 50", () => {
+    expect(greaterThanFifty(22)).toBe(false);
   });
 });

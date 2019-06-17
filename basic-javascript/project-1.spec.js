8551968fd25683deebd6ea5f5ff911b196ec90a7
Seth Lachman <lachmanseth17@gmail.com>
2019-06-17 17:57:24 -0400

    Added first two tests for project-1.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,18 @@
-const helpers = require('./project-1');
+const { multiplyByTen, subtractFive } = require("./project-1");
 
 // start testing!
+describe("Multiply by ten function", () => {
+  it("it should return a value multipled by 10", () => {
+    expect(multiplyByTen(5)).toBe(50);
+    expect(multiplyByTen(10)).toBe(100);
+    expect(multiplyByTen(0)).toBe(0);
+  });
+});
+
+describe("Subtract by 5 function", () => {
+  it("it should return a value subtracted by 5", () => {
+    expect(subtractFive(5)).toBe(0);
+    expect(subtractFive(15)).toBe(10);
+    expect(subtractFive(0)).toBe(-5);
+  });
+});
